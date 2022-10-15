176165c76f1f687205a604a0bd8afa04ebb8c8c1
Douglas Moraes <douglas.am@gmail.com>
2022-10-15 11:07:51 -0300

    3.16. Desafio: Lombok e repositório de restaurantes

---
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -0,0 +1,19 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Cozinha {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+}
---
+++ target/classes/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO cozinha (nome) VALUES ('Tailandesa');
+INSERT INTO cozinha (nome) VALUES ('Indiana');
+
+insert into restaurante (nome, taxa_frete) values ('Thai Gourmet', 10);
+insert into restaurante (nome, taxa_frete) values ('Thai Delivery', 9.50);
+insert into restaurante (nome, taxa_frete) values ('Tuk Tuk Comida Indiana', 15);
