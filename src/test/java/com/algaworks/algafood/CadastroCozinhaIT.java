9b011f5e36c0d24c1f285da5f5affe0aae3ba133
Douglas Moraes <douglas.am@gmail.com>
2023-04-19 21:24:57 -0300

    10.10. Criando um método para fazer setup dos testes

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -2,6 +2,8 @@ package com.algaworks.algafood;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -17,12 +19,16 @@ public class CadastroCozinhaIT {
 	@LocalServerPort
 	private int port;
 
+	@BeforeEach
+	public void setUp() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		RestAssured.port = port;
+		RestAssured.basePath = "/cozinhas";
+	}
+
 	@Test
 	public void deveRetornarStatus200_QuandoConsultarCozinhas() {
-		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 				given()
-					.basePath("/cozinhas")
-					.port(port)
 					.accept(ContentType.JSON)
 				.when()
 					.get()
@@ -32,10 +38,7 @@ public class CadastroCozinhaIT {
 
 	@Test
 	public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
-		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 			given()
-				.basePath("/cozinhas")
-				.port(port)
 				.accept(ContentType.JSON)
 			.when()
 				.get()
