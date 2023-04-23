342498bc2e34840a04c0efa703012cdb6079b4e2
Douglas Moraes <douglas.am@gmail.com>
2023-04-22 21:55:25 -0300

    11.7. Configurando e refatorando o projeto para usar UTC

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
@@ -1,13 +1,12 @@
 package com.algaworks.algafood.api.exceptionhandler;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-
 import lombok.Builder;
 import lombok.Getter;
+
+import java.time.OffsetDateTime;
+import java.util.List;
 
 @JsonInclude(Include.NON_NULL)
 @Getter
@@ -15,7 +14,7 @@ import lombok.Getter;
 public class Problem {
 
 	private Integer status;
-	private LocalDateTime timestamp;
+	private OffsetDateTime timestamp;
 	private String type;
 	private String title;
 	private String detail;
